43f121d3e29fcd43353a204da3907e0d7d5bec32
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-09-27 17:53:26 +0200

    power

--- commitnameversion.sh
+++ commitnameversion.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-rm -rf a.out
 git add .
 git commit -m "$1"
 git tag -a "exercises-$2-$3" -m "$3"
---
+++ my_pow/my_pow.c
@@ -0,0 +1,15 @@
+int my_pow(int a, int b)
+{
+    if(b == 1)
+    {
+        return a;
+    }
+    else if(b%2 == 0)
+    {
+        return my_pow(a, b/2)* my_pow(a, b/2);
+    }
+    else
+    {
+        return my_pow(a, b/2)* my_pow(a, b/2)*a;
+    }
+}
