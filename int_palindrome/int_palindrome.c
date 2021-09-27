bc574e666d1aabee936164175fc71182b219cc44
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-09-27 18:27:42 +0200

    ajout int palindrome

---
+++ int_palindrome/int_palindrome.c
@@ -0,0 +1,15 @@
+int int_palindrome(int n)
+{
+    int nombre = n;
+    int reverse = 0;
+    int reste;
+
+    while(n!=0)
+    {
+        reste = n%10;
+        reverse = reverse*10+reste;
+        n/=10;
+    }
+
+    return (reverse == nombre) ? 1 : 0;
+}
