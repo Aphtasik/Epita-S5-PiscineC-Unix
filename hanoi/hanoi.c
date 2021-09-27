74e565ee33d60ce50d47450c4d6c7f901128a911
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-09-27 21:26:12 +0200

    add hanoi

---
+++ hanoi/hanoi.c
@@ -0,0 +1,18 @@
+void hanoi_print(unsigned n, char d, char a, char i)
+{
+    if(n!=0)
+    {
+        hanoi_print(n-1, d, i, a);
+        putchar(d);
+        putchar('-');
+        putchar('>');
+        putchar(a);
+        putchar('\n');
+        hanoi_print(n-1, i, a, d);
+    }
+}
+
+void hanoi(unsigned n)
+{
+   hanoi_print(n, '1', '3', '2'); 
+}
