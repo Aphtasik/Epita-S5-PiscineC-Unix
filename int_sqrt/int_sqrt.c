a5867557ea33d65d81c65863251c244c63abe6cb
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-09-27 18:20:46 +0200

    ajout interger square root

---
+++ int_sqrt/int_sqrt.c
@@ -0,0 +1,24 @@
+int int_sqrt(int n)
+{
+    if(n<0)
+    {
+        return -1;
+    }
+    else if(n == 0 || n == 1)
+    {
+        return n;
+    }
+    else
+    {
+        int root = 1;
+        int i = 1;
+
+        while(root <= n)
+        {
+            i++;
+            root=i*i;
+        }
+    
+        return i-1;
+    }
+}
