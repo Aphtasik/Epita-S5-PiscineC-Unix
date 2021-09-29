7bae1aa25873f60d3c02dfb129784a1008132ae8
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-09-29 13:16:28 +0200

    stddef array max min

--- array_max_min/array_max_min.c
+++ array_max_min/array_max_min.c
@@ -1,17 +1,19 @@
+#include <stddef.h>
+
 void array_max_min(int tab[], size_t len, int *max, int *min)
 {
-    if (tab && len > 0) 
+    if (tab && len > 0)
     {
         *max = tab[0];
         *min = tab[0];
 
-        for (size_t i = 0; i < len; i++) 
+        for (size_t i = 0; i < len; i++)
         {
-            if (tab[i] < *min) 
+            if (tab[i] < *min)
             {
                 *min = tab[i];
             }
-            if (tab[i] > *max) 
+            if (tab[i] > *max)
             {
                 *max = tab[i];
             }
