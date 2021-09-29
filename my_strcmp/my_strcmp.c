be2a2420a269ff1f42a29269fb47cc90970f6efc
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-09-29 12:02:31 +0200

    reformat my_strcmp

--- my_strcmp/my_strcmp.c
+++ my_strcmp/my_strcmp.c
@@ -1,14 +1,14 @@
-int my_strcmp(const char *s1, const char *s2) 
+int my_strcmp(const char *s1, const char *s2)
 {
     int i = 0;
 
-    while (s1[i] != '\0' && s2[i] != '\0') 
+    while (s1[i] != '\0' && s2[i] != '\0')
     {
-        if ((s1[i] > s2[i]) || ((s1[i + 1] != '\0') && (s2[i + 1] == '\0'))) 
+        if ((s1[i] > s2[i]) || ((s1[i + 1] != '\0') && (s2[i + 1] == '\0')))
         {
             return 1;
-        } 
-        else if ((s1[i] < s2[i]) || ((s1[i + 1] == '\0') && (s2[i + 1] != '\0'))) 
+        }
+        else if ((s1[i] < s2[i]) || ((s1[i + 1] == '\0') && (s2[i + 1] != '\0')))
         {
             return -1;
         }
