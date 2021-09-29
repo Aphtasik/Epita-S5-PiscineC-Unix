f72b33900dc38f1a92623d0d7d09891e6314e88f
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-09-29 15:31:58 +0200

    add mult_mat

---
+++ int_mats_mult/mat_mult.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+#include "mat_mult.c"
+
+void mat_mult(int **mat1, int **mat2, size_t *matrices_size, int **out)
+{
+    for (size_t i = 0; i < *matrices_size; i++)
+    {
+        for (size_t j = 0; j < *matrices_size; j++)
+        {
+            out[i][j] = 0;
+            for (size_t k = 0; k < *matrices_size; k++)
+            {
+                out[i][j] += mat1[i][k] * mat[k][j];
+            }
+        }
+    }
+}
---
+++ int_vector_hill/int_vector_hill.c
@@ -0,0 +1,36 @@
+#include "int_vector_hill.h"
+#include <stddef.h>
+
+int int_vector_hill(struct int_vector vec)
+{
+    size_t left = 0;
+    size_t right = vec.size - 1;
+
+    while (left != right)
+    {
+        if (vec.data[left] < vec.data[right])
+        {
+            if (vec.data[left] <= vec.data[left + 1])
+            {
+                left++;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        else
+        {
+            if (vec.data[right - 1] >= vec.data[right])
+            {
+                right--;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+
+    return ((int)left);
+}
