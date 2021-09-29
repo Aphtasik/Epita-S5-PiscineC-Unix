8b93d1155f936e3426471d7694d7ad9d3e7737ce
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-09-29 14:19:07 +0200

    chg size_t vec_mac vec_print + add vec_insert_sort

---
+++ int_vector_insert_sort/int_vector_insert_sort.c
@@ -0,0 +1,30 @@
+#include "int_vector_insert_sort.h"
+#include <stddef.h>
+
+static struct int_vector swap(struct int_vector vec, size_t i, size_t j)
+{
+    int temp = vec.data[i];
+    data[i] = data[j];
+    data[j] = temp;
+
+    return vec;
+}
+
+struct int_vector int_vector_inser_sort(struct int_vector vec)
+{
+    size_t i;
+    size_t j;
+
+    for (i = 1; i < vec.size; i++)
+    {
+        j = i - 1;
+        while (j >= 0 && vec.data[i] < vec.data[j])
+        {
+            swap(vec, j, j + 1);
+            j--;
+        }
+        vec.data[j + 1] = vec.data[i];
+    }
+
+    return vec;
+}
--- int_vector_max/int_vector_max.c
+++ int_vector_max/int_vector_max.c
@@ -1,10 +1,11 @@
 #include "int_vector_max.h"
+#include <stddef.h>
 
 int int_vector_max(const struct int_vector vec)
 {
     int max = 0;
     
-    for (int i = 0; i < vec.size; i++)
+    for (size_t i = 0; i < vec.size; i++)
     {
         if (data[i] > max)
         {
