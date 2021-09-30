b45b4ac6090268ff58f41b0f6416bae55ee7ef9b
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-09-30 12:29:38 +0200

    clang intsqrt

--- int_sqrt/int_sqrt.c
+++ int_sqrt/int_sqrt.c
@@ -1,13 +1,13 @@
-int int_sqrt(int n) 
+int int_sqrt(int n)
 {
     if (n < 0)
     {
         return -1;
-    } 
+    }
     else if (n == 0 || n == 1)
     {
         return n;
-    } 
+    }
     else
     {
         int root = 1;
