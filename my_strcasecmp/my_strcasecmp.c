7aa3ddd13c89e0b5d67cc4eafe970183c600a735
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-09-30 13:20:59 +0200

    clange casecmp

--- my_strcasecmp/my_strcasecmp.c
+++ my_strcasecmp/my_strcasecmp.c
@@ -11,7 +11,7 @@ int my_strcasecmp(const char *s1, const char *s2)
         c1 = (s1[i] < 91 && s1[i] > 64) ? (s1[i] + 32) : (s1[i]);
         c2 = (s2[i] < 91 && s2[i] > 64) ? (s2[i] + 32) : (s2[i]);
         c11 = s1[i + 1];
-        c22 = s2[i + 1]; 
+        c22 = s2[i + 1];
 
         if ((c1 > c2) || ((c11 != '\0') && (c22 == '\0')))
         {
