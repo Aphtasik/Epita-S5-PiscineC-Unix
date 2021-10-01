4b46b5b3b1344337e35271ffc2e5c13f244a780e
Alexandre GAUTIER <alexandre.gautier@epita.fr>
2021-10-01 14:34:58 +0200

    display square + pow corr

--- my_pow/my_pow.c
+++ my_pow/my_pow.c
@@ -1,15 +1,19 @@
 int my_pow(int a, int b)
 {
-    if(b == 1)
+    if (b == 1)
     {
         return a;
     }
-    else if(b%2 == 0)
+    else if (b == 0)
     {
-        return my_pow(a, b/2)* my_pow(a, b/2);
+        return 1;
+    }
+    else if (b % 2 == 0)
+    {
+        return my_pow (a, b / 2) * my_pow(a, b / 2);
     }
     else
     {
-        return my_pow(a, b/2)* my_pow(a, b/2)*a;
+        return my_pow(a, b / 2) * my_pow(a, b / 2) * a;
     }
 }
