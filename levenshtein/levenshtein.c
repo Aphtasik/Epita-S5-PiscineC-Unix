0e6e5ae921d99b28ff509de204592b3e09ee8333
Alexandre GAUTIER <alexandre.gautier@epita.fr>
2021-10-02 13:00:43 +0200

    leven iter

--- levenshtein/levenshtein.c
+++ levenshtein/levenshtein.c
@@ -1,5 +1,6 @@
+#include "levenshtein.h"
+
 #include <stdlib.h>
-#include <stdio.h>
 #include <stddef.h>
 #define MIN(A, B) ((A) < (B) ? (A) : (B))
 
@@ -15,49 +16,32 @@ size_t len(const char *s)
 
 size_t levenshtein(const char *s1, const char *s2)
 {
+    size_t ld;
+    size_t od;
     size_t len_s1 = len(s1);
     size_t len_s2 = len(s2);
     size_t i = 0;
     size_t j = 0;
-    size_t sub_cost = 0;
 
-    size_t **distance = malloc(sizeof(size_t) * len_s1);
-    for (i = 0; i < len_s1; i++)
+    size_t *distance = calloc(len_s1 + 1, sizeof(size_t));
+
+    for (i = 1; i <= len_s1; i++)
     {
-        size_t *cols = calloc(len_s2, sizeof(size_t));
-        distance[i] = cols;
+        distance[i] = i;
     }
 
-    for (i = 1; i < len_s1; i++)
+    for (j = 1; j <= len_s2; j++)
     {
-        distance[i][0] = i;
-    }
-
-    for (i = 1; i < len_s2; i++)
-    {
-        distance[0][j] = j;
-    }
-
-    for (j = 1; j < len_s2; j++)
-    {
-        for (i = 1; i < len_s1; i++)
+        distance[0] = j;
+        ld = j - 1;
+        for (i = 1, ld = j - 1; i <= len_s1; i++)
         {
-            if (s1[i] == s2[j])
-            {
-                sub_cost = 0;
-            }
-            else
-            {
-                sub_cost = 1;
-            }
-            size_t min =MIN(distance[i - 1][j] + 1, distance[i][j - 1] + 1);
-            distance[i][j] =MIN(min, distance[i - 1][j - 1] + sub_cost);
+            od = distance[i];
+            size_t temp = MIN(distance[i] + 1, distance[i - 1] + 1);
+            size_t is_eq = (s1[i - 1] == s2[j - 1]) ? 0 : 1;
+            distance[i] = MIN(temp, ld + is_eq); 
+            ld = od;
         }
     }
-    return distance[len_s1][len_s2];
-}
-
-int main()
-{
-    printf("%ld\n", levenshtein("doom", "tools"));
+    return distance[len_s1];
 }
