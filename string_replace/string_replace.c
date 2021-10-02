3d3703d3eea75bca1575f6245f9f70704774635b
Alexandre GAUTIER <alexandre.gautier@epita.fr>
2021-10-02 13:46:15 +0200

    add string replace

---
+++ string_replace/string_replace.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+
+#include "string_replace.h"
+
+int len(const char *str)
+{
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+    return i;
+}
+
+int countc(const char *str, int len, char c)
+{
+    int count = 0;
+
+    for(int i = 0; i < len; i++)
+    {
+        if (str[i] == c)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+char *string_replace(char c, const char *str, const char *pattern)
+{
+    if (!str)
+        return NULL;
+    if (!pattern)
+        pattern = "";
+
+    int len_str = len(str);
+    int len_pattern = len(pattern);
+    int count = countc(str, len_str, c);
+    int new_i = 0;
+
+    char *new = malloc(sizeof(char) * (len_str + (count * len_pattern)));
+
+    for (int i = 0; i < len_str ; i++)
+    {
+        if (str[i] == c)
+        {
+            for (int j = 0; j < len_pattern; j++)
+            {
+                new[new_i] = pattern[j];
+                new_i++;
+            }
+        }
+        else
+        {
+            new[new_i] = str[i]; 
+            new_i++;
+        }
+    }
+    return new;
+}
