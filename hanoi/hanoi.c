8c0ecd316227f33d0212fe64196773c9e4ab9d18
Alexandre GAUTIER <alexandre.gautier@epita.fr>
2021-10-02 21:37:01 +0200

    hanoi clang

--- hanoi/hanoi.c
+++ hanoi/hanoi.c
@@ -1,18 +1,20 @@
+#include <stdio.h>
+
 void hanoi_print(unsigned n, char d, char a, char i)
 {
-    if(n!=0)
+    if (n != 0)
     {
-        hanoi_print(n-1, d, i, a);
+        hanoi_print(n - 1, d, i, a);
         putchar(d);
         putchar('-');
         putchar('>');
         putchar(a);
         putchar('\n');
-        hanoi_print(n-1, i, a, d);
+        hanoi_print(n - 1, i, a, d);
     }
 }
 
 void hanoi(unsigned n)
 {
-   hanoi_print(n, '1', '3', '2'); 
+   hanoi_print(n, '1', '3', '2');
 }
