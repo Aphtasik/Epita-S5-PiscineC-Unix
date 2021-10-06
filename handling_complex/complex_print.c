e474592b2a15abee053fd2c6ee931650b3001cfa
Alexandre GAUTIER <alexandre.gautier@epita.fr>
2021-10-06 21:23:50 +0200

    handling compl fix print

--- handling_complex/complex_print.c
+++ handling_complex/complex_print.c
@@ -1,16 +1,18 @@
 #include "complex_print.h"
-
-#include <stdio.h>
-
 #include "complex.h"
 #include "complex_operations.h"
 
+#include <stdio.h>
+
 void print_complex(struct complex a)
 {
-    if (a.img > 0)
+    if (a.img >= 0)
     {
         printf("complex(%.2f + %.2fi)\n");
     }
-    a.img = -a.img;
-    printf("complex(%.2f - %.2fi)\n");
+    else
+    {
+        a.img = -a.img;
+        printf("complex(%.2f - %.2fi)\n");
+    }
 }
