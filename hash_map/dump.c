51f37bf167869859763c93583ca7acd735cd7049
Alexandre GAUTIER <alexandre.gautier@epita.fr>
2021-10-06 18:38:44 +0200

    hash files

---
+++ hash_map/dump.c
@@ -0,0 +1,26 @@
+#include "hash_map.h"
+
+#include <stdio.h>
+#include <stddef.h>
+
+void hash_map_dump(struct hash_map *hash_map)
+{
+    if (hash_map)
+    {
+        for (size_t i = 0; i < hash_map->size; i++)
+        {
+            struct pair_list *pair = hash_map->data[i];
+            if (!pair)
+            {
+                continue;
+            }
+            printf("%s: %s", pair->key, pair->value);
+            while (pair->next)
+            {
+                pair = pair -> next;
+                printf("%s: %s", pair->key, pair->value);
+            }
+            printf("\n");
+        }
+    }
+}
