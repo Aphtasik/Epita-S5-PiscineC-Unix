7dccd5363cacfa4dd0a8487bb2223c7a1d99e7cf
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-10-13 10:55:02 +0200

    print handlig

--- handling_complex/complex_print.c
+++ handling_complex/complex_print.c
@@ -9,11 +9,11 @@ void print_complex(struct complex a)
 {
     if (a.img >= 0)
     {
-        printf("complex(%.2f + %.2fi)\n");
+        printf("complex(%.2f + %.2fi)\n", a.real, a.img);
     }
     else
     {
         a.img = -a.img;
-        printf("complex(%.2f - %.2fi)\n");
+        printf("complex(%.2f - %.2fi)\n", a.real, a.img);
     }
 }
