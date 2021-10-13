63d9e7c678566aa34c717714ea97c9e4c98dc667
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-10-13 13:38:05 +0200

    test a bit pow

--- test_a_bit/is_set.c
+++ test_a_bit/is_set.c
@@ -1,14 +1,25 @@
-#include <stdio.h>
+int my_pow(int a, int b)
+{
+    if (b == 1)
+    {
+        return a;
+    }
+    else if (b == 0)
+    {
+        return 1;
+    }
+    else if (b % 2 == 0)
+    {
+        return my_pow(a, b / 2) * my_pow(a, b / 2);
+    }
+    else
+    {
+        return my_pow(a, b / 2) * my_pow(a, b / 2) * a;
+    }
+}
 
 unsigned int is_set(unsigned int value, unsigned char n)
 {
-    unsigned short flag_set = 1 << n;
-    return value & flag_set;
-}
-
-int main()
-{
-    printf("%d\n", is_set(24, 4));
-    printf("%d\n", is_set(24, 3));
-    return 0;
+    unsigned short flag_set = 1 << (n - 1);
+    return (value & flag_set) == (my_pow(2, n - 1));
 }
