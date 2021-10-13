1a41ccf767d14a8aa759a6e772cb5b389603c128
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-10-13 10:35:43 +0200

    int vice maxÃ cond add

--- int_vector_vice_max/int_vector_vice_max.c
+++ int_vector_vice_max/int_vector_vice_max.c
@@ -1,18 +1,35 @@
 #include "int_vector_vice_max.h"
 
-#include <stddef.h>
-
 int int_vector_vice_max(const struct int_vector vec)
 {
     int max = vec.data[0];
-    int vmax = vec.data[1];
+    int vmax = vec.data[0];
+    size_t i;
 
-    for (size_t i = 1; i < vec.size; i++)
+    if (vec.size < 2)
+    {
+        return max;
+    }
+
+    for (i = 1; i < vec.size; i++)
     {
         if (vec.data[i] > max)
         {
             vmax = max;
             max = vec.data[i];
+        }
+    }
+
+    if (max == vec.data[0])
+    {
+        vmax = vec.data[1];
+    }
+
+    for (i = 0; i < vec.size; i++)
+    {
+        if (vec.data[i] > vmax && vec.data[i] != max)
+        {
+            vmax = vec.data[i];
         }
     }
 
