aa98dd7cb7238b322dc74c93a4b03bdf515e77f6
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-10-23 16:23:44 +0200

    insert sort add

---
+++ insertion_sort/insertion_sort.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+typedef int (*f_cmp)(const void *, const void *);
+
+void insertion_sort(void **array, f_cmp comp)
+{
+    int i;
+    int j;
+    void *key;
+
+    for (i = 1; array[i]; i++)
+    {
+        key = array[i];
+        j = i - 1;
+
+        while (j >= 0 && (comp(array[j], key) == 1))
+        {
+            array[j + 1] = array[j];
+            j = j - 1;
+        }
+        array[j + 1] = key;
+    }
+}
