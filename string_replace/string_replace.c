7ddfbab6dc51683c2398582f261d4da0a8c47a30
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-11-09 08:54:09 +0100

    string replace math help

--- string_replace/string_replace.c
+++ string_replace/string_replace.c
@@ -1,6 +1,7 @@
 #include "string_replace.h"
 
 #include <stdlib.h>
+#include <stdio.h>
 
 int len(const char *str)
 {
@@ -30,21 +31,19 @@ char *string_replace(char c, const char *str, const char *pattern)
 {
     if (!str)
         return NULL;
-    if (!pattern)
-        pattern = "";
 
     int len_str = len(str);
     int len_pattern = len(pattern);
     int count = countc(str, len_str, c);
     int new_i = 0;
 
-    char *new = malloc(sizeof(char) * (len_str + (count * len_pattern)));
+    char *new = malloc(sizeof(char) * (len_str - count + (count * len_pattern + 1)));
 
     for (int i = 0; i < len_str; i++)
     {
         if (str[i] == c)
         {
-            for (int j = 0; j < len_pattern; j++)
+            for (int j = 0; j < len_pattern && pattern; j++)
             {
                 new[new_i] = pattern[j];
                 new_i++;
