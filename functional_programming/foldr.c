8091dd0f16f22443bfe933b0d644e02441a17bce
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-11-15 14:50:57 +0100

    functionnal fix

--- functional_programming/foldr.c
+++ functional_programming/foldr.c
@@ -3,7 +3,7 @@
 int foldr(int *array, size_t len, int (*func)(int, int))
 {
     int acc = 0;
-    int i;
+    int i = 1;
     for (; (len - i) > 0; i++)
     {
         acc = func(array[len - i], acc);
