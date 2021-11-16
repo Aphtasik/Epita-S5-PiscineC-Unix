bd3b413c090ab8d3f96cb88591da4a59aed40ace
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-11-16 19:39:53 +0100

    memcmp push

---
+++ my_memcmp/my_memcmp.c
@@ -0,0 +1,18 @@
+#include "my_memcmp.h"
+
+int my_memcmp(const void *s1, const void *s2, size_t num)
+{
+    size_t si = 0;
+
+    const char *cs1 = s1;
+    const char *cs2 = s2;
+
+    for (size_t i = 0; i < num; i++)
+    {
+        if (cs1[si] < cs2[si])
+            return -1;
+        else if (cs1[si] > cs2[si])
+            return 1;
+    }
+    return 0;
+}
