3e79903d0b02ebe7bd8f78af8dabd303097ad45c
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-11-16 09:42:37 +0100

    eat_r ez

--- eat_r/eat_r.sh
+++ eat_r/eat_r.sh
@@ -2,5 +2,11 @@
 
 for var in "$@" 
 do
-    echo "$var" | tr -d 'rR'
+    if [ $var = ${@: -1} ]
+    then
+        echo "$var" | tr -d 'rR'
+    else
+        echo -n "$var, " | tr -d 'rR'
+    fi
 done
+
