762fa4f249df0b96825ddf873cca2740f50cd156
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-11-17 11:24:38 +0100

    rottt

---
+++ rotx/rotx.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+void rot_x(char *s, int x)
+{
+    if (s)
+    {
+        int i = 0;
+        if (x <= -26)
+        {
+            x = -x;
+            x = x % 26;
+            x = -x;
+        }
+
+        while (s[i] != '\0')
+        {
+            if (s[i] <= 'Z' && s[i] >= 'A')
+            {
+                if (x > 0)
+                    s[i] = (s[i] + x - 65) % 26 + 65;
+                else
+                    s[i] = (s[i] + x - 65 + 26) % 26 + 65;
+            }
+            else if (s[i] <= 'z' && s[i] >= 'a')
+            {
+                if (x > 0)
+                    s[i] = (s[i] + x - 97) % 26 + 97;
+                else
+                    s[i] = (s[i] + x - 97 + 26) % 26 + 97;
+            }
+            else if (s[i] <= '0' && s[i] >= '9')
+            {
+                if (x > 0)
+                    s[i] = (s[i] + x - '0') % 26 + 65;
+                else
+                    s[i] = (s[i] + x - '0' + 26) % 26 + 65;
+            }
+            i++;
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+        return 0;
+
+    int x = atoi(argv[1]);
+    char str[2048] =  { 0 };
+    size_t nread = read(0, str, 2048);
+    int i = 0;
+
+    rot_x(str, x);
+
+    write(1, str, nread);
+
+    return 1;
+}
