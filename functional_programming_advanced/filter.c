3db4a4c84d5691b5c2684cbd5f4cde41f603d840
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-11-18 13:55:21 +0100

    fun adv

--- functional_programming_advanced/filter.c
+++ functional_programming_advanced/filter.c
@@ -5,13 +5,15 @@
 size_t filter(int *array, size_t len, int **out_array, bool (*func)(int))
 {
     *out_array = malloc(sizeof(int) * len);
-    int out_i = 0;
+    if (!(*out_array))
+        return 0;
 
+    int out_i = 0;
     for (size_t i = 0; i < len; i++)
     {
         if (func(array[i]))
         {
-            *out_array[out_i] = array[i];
+            *(*out_array + out_i) = array[i];
             out_i++;
         }
     }
