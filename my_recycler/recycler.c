4135eb68ccc9ab51320313b7e21eb4609e663815
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-11-18 11:46:22 +0100

    recycl p

--- my_recycler/recycler.c
+++ my_recycler/recycler.c
@@ -1,4 +1,6 @@
-#include "recylcler.h"
+#include "recycler.h"
+
+#include <stdlib.h>
 
 struct recycler *recycler_create(size_t block_size, size_t total_size)
 {
@@ -10,17 +12,61 @@ struct recycler *recycler_create(size_t block_size, size_t total_size)
 
     struct recycler *r = malloc(sizeof(struct recycler));
     if (!r)
-    {
         return NULL;
-    }
 
     struct free_list *f = malloc(sizeof(struct free_list));
     if (!f)
-    {
         return NULL;
-    }
+
     f->next = NULL;
 
     r->block_size = block_size;
-    r->capacity = total
+    r->chunk = NULL;
+    r->free = NULL;
+    r->capacity = 0;
+
+    return r;
+}
+
+void recycler_destroy(struct recycler *r)
+{
+    if (!r)
+        return;
+
+    struct free_list *f = r->free;
+    struct free_list *tmp;
+
+    while (r)
+    {
+        tmp = f;
+        f = f->next;
+        free(tmp);
+    }
+
+    r->capacity = 0;
+    r->free = NULL;
+}
+
+void *recycler_allocate(struct recycler *r)
+{
+    if (!r || !r->free)
+        return NULL;
+
+    struct free_list *f = r->free;
+    struct free_list *tmp = f;
+    f = f->next;
+
+    return tmp;
+}
+
+void recycler_free(struct recycler *r, void *block)
+{
+    if (!r | !block)
+        return;
+
+    struct free_list *f = r->free;
+
+    struct free_list *new = block;
+    new->next = f;
+    r->free = new;
 }
