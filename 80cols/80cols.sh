e9cb08d3ced5e847c2ce531d8d45642ca61ebb3d
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-11-19 16:24:12 +0100

    80 col

--- 80cols/80cols.sh
+++ 80cols/80cols.sh
@@ -8,9 +8,10 @@ if [ ! -f "$1" ]; then
     exit 1
 fi
 
-filename=$1
 while IFS=read line; do
     var=$(echo -n "$line" | wc -c)
-    [ "$var" -ge 80 ] && echo "$line"
-done < "$filename"
+    if [ "$var" -ge 80 ]; then
+        echo "$line"
+    fi
+done < "$1"
 
