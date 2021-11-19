503f3a2ed5117afeba64cd9fdfa6ab72b3c8de98
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-11-19 17:44:55 +0100

    mix

--- mix_files/mix_files.sh
+++ mix_files/mix_files.sh
@@ -1,10 +1,37 @@
 #!/bin/sh
 
-if [ $# -ne 3 ]; then
+if [ $# -ne 2 ]
+then
+    echo "Usage: mix_files file1 file2" >&2
+    exit 1
+fi
 
-if 
-while IFS=$'\t' read -r f1 f2
+if [ ! -f "$1" ] && [ ! -f "$2" ]
+then
+    exit 1
+fi
+
+nb1=$(cat "$1" | wc -l)
+min=$nb1
+nb2=$(cat "$2" | wc -l)
+max=$nb2
+maxfile=$2
+i=1
+
+if [ $nb1 -gt $nb2 ]
+then
+    maxfile=$1
+    max=$nb1
+    min=$nb2
+fi
+
+while [ $i -ne $((min + 1)) ]
 do
-  printf '%s\n' "$f1"
-  printf '%s\n' "$f2"
-done < <(paste $1 $2)
+    head -n $i "$1" | tail -n 1
+    head -n $i "$2" | tail -n 1
+    ((i++))
+done
+
+tail -n $((max - i + 1)) $maxfile
+
+exit 0
