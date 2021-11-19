a977bf273f43abd1b824124665a35b4bbb5aaa74
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-11-19 15:52:33 +0100

    80 col

--- 80cols/80cols.sh
+++ 80cols/80cols.sh
@@ -14,9 +14,8 @@ while read line; do
     var="$(($(printf %s "$line" | wc -m)))"
     if [ "$line" == "$string"]; then
         echo "This line is longer that 80 cols because it contains a \\n so you may consider it smaller than 80 cols"
+    elif [ "$var" -ge 80 ]; then
+        echo "$line"
     fi
-    #if [ "$var" -ge 80 ]; then
-     #   echo "$line"
-    #fi
 done < "$filename"
 
