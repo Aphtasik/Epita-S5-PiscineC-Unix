c5ad11cec52a22131978c7a8e48df236f79739c7
Alexandre Gautier <alexandre.gautier@epita.fr>
2021-11-20 15:04:03 +0100

    ksjagsqod

--- most_used/most_used.sh
+++ most_used/most_used.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-# history | tr -s ' ' | cut -d ' ' -f3 | sort | uniq -c | sort -r | head
-# echo "$(<~/.bash_history)" | tr -s ' ' | cut -d ' ' -f3 | sort | uniq -c | sort -r | head
-cut -d ' ' -f 1 ~/.bash_history | sort | uniq -c | sort -nr | head  -n 10
+echo "$(<~/.bash_history)" | tr -s ' ' | cut -d ' ' -f3 | sort | uniq -c | sort -r | head | tr -s " " 
 
